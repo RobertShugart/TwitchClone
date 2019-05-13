74aa4426b60e9ac419055f3ce7cb7be78592337c
RobertS <rshugart@protonmail.com>
2019-05-13 10:54:29 -0400

    Created streams paths and header

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
-
-const PageOne = () => {
-  return <div>PageOne</div>
-};
-
-const PageTwo = () => {
-  return (
-    <div>
-      PageTwo
-      <button>Click Me</button>
-    </div>
-  );
-};
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './header';
 
 
 const App = () => {
   return (
     <div>
+      <Header />
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/pagetwo" component={PageTwo} />
+          <Route path='/' exact component={StreamList} />
+          <Route path='/streams/new' exact component={StreamCreate} />
+          <Route path='/streams/edit' exact component={StreamEdit} />
+          <Route path='/streams/delete' exact component={StreamDelete} />
+          <Route path='/streams/show' exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
