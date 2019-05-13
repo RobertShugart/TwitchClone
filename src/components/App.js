d73ce20c813b80ccb27b8eceb08a3dbe41ba5c00
RobertS <rshugart@protonmail.com>
2019-05-13 12:29:29 -0400

    Added some styling to Header and placed in BrowserRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
@@ -10,10 +10,10 @@ import Header from './header';
 
 const App = () => {
   return (
-    <div>
-      <Header />
+    <div className='ui containter'>
       <BrowserRouter>
         <div>
+          <Header />
           <Route path='/' exact component={StreamList} />
           <Route path='/streams/new' exact component={StreamCreate} />
           <Route path='/streams/edit' exact component={StreamEdit} />
