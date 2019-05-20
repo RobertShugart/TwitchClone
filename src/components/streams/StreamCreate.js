550dc4c27674d7328ce2e861ed536c32885633fe
RobertS <rshugart@protonmail.com>
2019-05-20 11:48:26 -0400

    Added css className = ui form

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -14,7 +14,7 @@ class StreamCreate extends React.Component {
 
   render() {
     return (
-      <form>
+      <form className="ui form">
         {/* Field is apart of redux-form, it's some type of Field to get input from the user  */}
         <Field name="title" component={this.renderInput} label="Enter Title" />
         <Field name="description" component={this.renderInput} label="Enter Description" />
