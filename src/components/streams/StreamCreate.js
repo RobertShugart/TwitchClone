c4a3f8547c05c05532dd1046e79380a7912836b6
RobertS <rshugart@protonmail.com>
2019-05-20 12:08:59 -0400

    Create validation and error message

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -11,17 +11,36 @@ class StreamCreate extends React.Component {
     );
   }
 
+  onSubmit(event) {
+
+  }
+
 
   render() {
     return (
-      <form className="ui form">
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
         {/* Field is apart of redux-form, it's some type of Field to get input from the user  */}
         <Field name="title" component={this.renderInput} label="Enter Title" />
         <Field name="description" component={this.renderInput} label="Enter Description" />
+        <button className="ui button primary">Submit</button>
       </form>
     );
   }
 }
+
+const validate = (formValues) => {
+  const errors = {};
+
+  if (!formValues.title) {
+    errors.title = "You must enter a title";
+  }
+
+  if (!formValues.description) {
+    errors.description = "You must enter in a description";
+  }
+
+  return errors;
+};
 
 export default reduxForm({
   form: 'streamCreate'
