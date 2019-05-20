b67e28d3a7946492f0cbf61b32f58b68612ca146
RobertS <rshugart@protonmail.com>
2019-05-20 13:23:24 -0400

    Changed renderInput into arrorFunction to solve this. issue

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -12,7 +12,7 @@ class StreamCreate extends React.Component {
     }
   }
 
-  renderInput({ input, label, meta }) {
+  renderInput = ({ input, label, meta }) => {
     return (
       <div className="field">
         <label>{label}</label>
