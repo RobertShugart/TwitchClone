87cecc30e796d0fbb52a8f1ff3c6cdd420606cde
RobertS <rshugart@protonmail.com>
2019-05-20 19:59:48 -0400

    Created helper function and sylized error message

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
-  renderError({ error, touch }) {
-    if (touch && error) {
+  renderError({ error, touched }) {
+    if (touched && error) {
       return (
         <div className="ui error message">
           <div className="header">{error}</div>
@@ -13,8 +13,9 @@ class StreamCreate extends React.Component {
   }
 
   renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
     return (
-      <div className="field">
+      <div className={className}>
         <label>{label}</label>
         <input {...input} />
         {this.renderError(meta)}
@@ -43,11 +44,11 @@ const validate = (formValues) => {
   const errors = {};
 
   if (!formValues.title) {
-    errors.title = "You must enter a title";
+    errors.title = 'You must enter a title';
   }
 
   if (!formValues.description) {
-    errors.description = "You must enter in a description";
+    errors.description = 'You must enter in a description';
   }
 
   return errors;
