793b20b2885dad0fba9ef11810dd15ede990adc5
RobertS <rshugart@protonmail.com>
2019-05-21 12:21:34 -0400

    Connected ActionCreater to StreamCreate.js

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions/';
 
 class StreamCreate extends React.Component {
   renderError({ error, touched }) {
@@ -54,7 +56,9 @@ const validate = (formValues) => {
   return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: 'streamCreate',
   validate
 })(StreamCreate);
+
+export default connect()(formWrapped);
