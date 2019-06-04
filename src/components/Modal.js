2380afa8d6586a9156182ebbff421d08afefedaa
RobertS <rshugart@protonmail.com>
2019-06-04 15:41:13 -0400

    Added #modal to creatPortal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -5,7 +5,8 @@ const Modal = (props) => {
 	return ReactDOM.createPortal(
 		<div className="ui dimmer modals visible active">
 			<div className="ui standard modal visible active">Modal Must be in a portal in React</div>
-		</div>
+		</div>,
+		document.querySelector('#modal')
 	);
 };
 
